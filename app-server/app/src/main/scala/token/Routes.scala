1970d6515e4c9d7db205d33b7d5a490d1d1cf633
Terence Tuhinanshu <ttuhinanshu@azavea.com>
2016-10-13 17:43:04 -0400

    Add authorization endpoint
    Once the user has a refresh token, they can POST to /api/tokens a
    simple JSON structure like this:
    
      {
        refresh_token: "Agmv..."
      }
    
    If the token is valid they will get back a structure like this:
    
      {
        expires_in: 36000,
        id_token: "eyJ...",
        token_type: "Bearer"
      }

--- app-server/app/src/main/scala/token/Routes.scala
+++ app-server/app/src/main/scala/token/Routes.scala
@@ -30,6 +30,12 @@ trait TokenRoutes extends Authentication
   case class DeviceCredential(id: String, device_name: String)
   implicit val deviceCredentialJson = jsonFormat2(DeviceCredential)
 
+  case class AuthorizedToken(id_token: String, expires_in: Int, token_type: String)
+  implicit val authorizedTokenJson = jsonFormat3(AuthorizedToken)
+
+  case class RefreshToken(refresh_token: String)
+  implicit val refreshTokenJson = jsonFormat1(RefreshToken)
+
   val uri = Uri(s"https://$auth0Domain/api/v2/device-credentials")
   val headers = List(
     Authorization(GenericHttpCredentials("Bearer", auth0Bearer))
@@ -48,6 +54,28 @@ trait TokenRoutes extends Authentication
 
             onSuccess(req) {
               case HttpResponse(OK, _, entity, _) => complete(Unmarshal(entity).to[List[DeviceCredential]])
+              case HttpResponse(errCode, _, _, _) => complete(errCode)
+            }
+          }
+        } ~
+        post {
+          entity(as[RefreshToken]) { rt =>
+            val params = FormData(
+              "api_type" -> "app",
+              "grant_type" -> "urn:ietf:params:oauth:grant-type:jwt-bearer",
+              "scope" -> "openid",
+              "refresh_token" -> rt.refresh_token,
+              "client_id" -> auth0ClientId,
+              "target" -> auth0ClientId
+            ).toEntity
+            val req = Http().singleRequest(HttpRequest(
+              method = POST,
+              uri = uri.withPath(Path("/delegation")),
+              entity = params
+            ))
+
+            onSuccess(req) {
+              case HttpResponse(OK, _, entity, _) => complete(Unmarshal(entity).to[AuthorizedToken])
               case HttpResponse(errCode, _, _, _) => complete(errCode)
             }
           }
